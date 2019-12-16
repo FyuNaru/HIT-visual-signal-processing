d558821d34678abf21629493356e29086e125591
FyuNaru <jywang1998@outlook.com>
2019-12-16 19:29:59 +0800

    修改了一个bug

--- Vision-lab2/main2.py
+++ Vision-lab2/main2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 def IdealHighPassFiltering(f_shift):
     # 设置滤波半径
-    D0 = 30
+    D0 = 8
     # 初始化
     m = f_shift.shape[0]
     n = f_shift.shape[1]
@@ -14,14 +14,14 @@ def IdealHighPassFiltering(f_shift):
     for i in range(m):
         for j in range(n):
             D = np.sqrt((i - x0)**2 + (j - y0)**2)
-            if D > D0:
+            if D >= D0:
                 h1[i][j] = 1
     result = np.multiply(f_shift, h1)
     return result
 
 def GaussLowPassFiltering(f_shift):
     # 设置滤波半径
-    D0 = 30
+    D0 = 8
     # 初始化
     m = f_shift.shape[0]
     n = f_shift.shape[1]
@@ -50,13 +50,13 @@ plt.imshow(p, 'gray')
 # 理想高通滤波
 IHPF = IdealHighPassFiltering(f_shift)
 new_f1 = np.fft.ifftshift(IHPF)
-new_image1 = np.uint8(np.real(np.fft.ifft2(new_f1)))
+new_image1 = np.uint8(np.abs(np.fft.ifft2(new_f1)))
 plt.subplot(2,2,3)
 plt.imshow(new_image1, 'gray')
 # 高斯低通滤波
 GLPF = GaussLowPassFiltering(f_shift)
 new_f2 = np.fft.ifftshift(GLPF)
-new_image2 = np.uint8(np.real(np.fft.ifft2(new_f2)))
+new_image2 = np.uint8(np.abs(np.fft.ifft2(new_f2)))
 plt.subplot(2,2,4)
 plt.imshow(new_image2, 'gray')
 plt.show()
